054cadca3c53b4b5b04de7e0ac8c6377fa94a6df
nydegang <149098880@qq.com>
2018-12-09 15:08:36 +0800

    南阳德刚修改了业务类

--- src/main/java/com/oraclewdp/ddbookmarket/biz/AdminBiz.java
+++ src/main/java/com/oraclewdp/ddbookmarket/biz/AdminBiz.java
@@ -3,5 +3,10 @@ package com.oraclewdp.ddbookmarket.biz;
 import com.oraclewdp.ddbookmarket.model.Admin;
 
 public interface AdminBiz {
+    /**
+     * 根据用户名和密码查找用户
+     * @param admin
+     * @return
+     */
     boolean findByNameAndPwd(Admin admin);
 }
